8c97312a9fa90ec6abe65a88cc797d41af49382e
Jorge Abreu <j.ferraz.de.abreu@avanade.com>
2022-10-10 17:29:08 -0300

    feat: Scripts Foi criada a função Realiza conta.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,16 @@
 function Calculadora() {
     this.display = document.querySelector('.display');
     
-    this.inicia = () => this.capturaCliques();
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
+    };
 
+    this.capturaEnter = () => {
+        document.addEventListener('keyup', e => {
+            if (e.keyCode === 13) this.realizaConta();
+        });
+    };
 
     this.capturaCliques = () =>{
         document.addEventListener('click', event => {
@@ -18,6 +26,22 @@ function Calculadora() {
 
     this.clear = () => this.display.value = '';
     this.deleta = () => this.display.value = this.value.slice(0,-1);
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
+
+            if (!conta) {
+                alert('Conta invalida');
+                return;
+            }
+
+            this.display.value = conta;
+
+        } catch (error) {
+            alert('Conta invalida');
+                return;
+        }
+    };
 
 }
 
